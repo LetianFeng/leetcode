51687d4c5eedf9ade865f902c9d02c2f55b59cec
Letian Feng <letian.feng@hotmail.com>
2017-04-01 11:06:03 +0200

    optimized solution 005

--- src/Solution005.java
+++ src/Solution005.java
@@ -1,42 +1,47 @@
 public class Solution005 {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() == 0)
+        if (s == null)
             return null;
+
+        if (s.length() == 0)
+            return s;
 
         int size = s.length();
 
-        String result = "";
+        for (int start = 0, end = size, subsize = size; subsize > 0; subsize--) {
 
-        for (int start = 0, end = 0; start < size; end++) {
-            String sub = s.substring(start, end);
+            while (end <= size) {
+                String sub = s.substring(start, end);
 
-            if(isPalindromic(sub) && sub.length() > result.length()) {
-                result = sub;
-            }
-            if (end >= size) {
+                if (isPalindromic(sub) && sub.length() > 0)
+                    return sub;
+
                 start++;
-                end = start;
+                end++;
             }
+
+            start = 0;
+            end = subsize - 1;
         }
 
-        return result;
+        return "";
     }
 
     private boolean isPalindromic(String s) {
-        if(s == null || s.length() < 2)
+        if (s == null || s.length() < 2)
             return true;
 
 
         int size = s.length();
-        for(int i = 0; i < size/2; i++) {
-            if (s.charAt(i) != s.charAt(size  - 1 - i))
+        for (int i = 0; i < size / 2; i++) {
+            if (s.charAt(i) != s.charAt(size - 1 - i))
                 return false;
         }
         return true;
     }
 
     public static void main(String[] args) {
-        String input = "babad";
+        String input = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
         Solution005 sol = new Solution005();
 
         System.out.println(sol.longestPalindrome(input));
