2479f4f261f6e82242136a5967a2dd635ad268f3
Letian Feng <letian.feng@hotmail.com>
2017-04-02 14:35:08 +0200

    initial solution 048

---
+++ src/Solution048.java
@@ -0,0 +1,26 @@
+public class Solution048 {
+    public void rotate(int[][] matrix) {
+        int size = matrix.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size - i - 1; j++) {
+                int leftUp = matrix[i][j];
+                matrix[i][j] = matrix[size - j - 1][i];
+                matrix[size - j - 1][i] = matrix[size - i - 1][size - j - 1];
+                matrix[size - i - 1][size - j - 1] = matrix[j][size - i - 1];
+                matrix[j][size - i - 1] = leftUp;
+            }
+        }
+
+        for (int[] col : matrix) {
+            for (int num : col)
+                System.out.print(num + " ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution048 sol = new Solution048();
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        sol.rotate(matrix);
+    }
+}
