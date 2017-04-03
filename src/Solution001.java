2141f3400cb920fd8508f9a7fb5ccad60166b89d
Letian Feng <letian.feng@hotmail.com>
2017-04-03 15:55:50 +0200

    optimized solution 001

--- src/Solution001.java
+++ src/Solution001.java
@@ -1,7 +1,27 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
-/**
- * Created by letian on 4/3/17.
- */
 public class Solution001 {
+    public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int i = 0;
+        for (int num : numbers) {
+            if (!map.containsKey(num))
+                map.put(target - num, i);
+            else
+                return new int[]{map.get(num), i};
+            i++;
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2,7,11,5};
+        int target = 9;
+
+        Solution001 sol = new Solution001();
+        int[] result = sol.twoSum(numbers, target);
+        for (int num : result)
+            System.out.print(num + " ");
+    }
 }
