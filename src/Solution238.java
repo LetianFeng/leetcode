6a566e006ab6e763c7199125d860614994a67381
Letian Feng <letian.feng@hotmail.com>
2017-04-05 16:05:09 +0200

    solution 238

---
+++ src/Solution238.java
@@ -0,0 +1,24 @@
+public class Solution238 {
+    public int[] productExceptSelf(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return nums;
+
+        if (nums.length == 1)
+            return new int[]{1};
+
+        int[] res = new int[nums.length];
+        res[0] = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+
+        return res;
+    }
+}
