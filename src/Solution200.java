bd1037783535fcbdbc12c0bc597ee17db216685c
Letian Feng <letian.feng@hotmail.com>
2017-04-06 11:00:40 +0200

    solution 200

---
+++ src/Solution200.java
@@ -0,0 +1,30 @@
+public class Solution200 {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0)
+            return 0;
+
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    destroyIsland(grid, i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void destroyIsland(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0')
+            return;
+
+        grid[i][j] = '0';
+        destroyIsland(grid, i - 1, j);
+        destroyIsland(grid, i + 1, j);
+        destroyIsland(grid, i, j - 1);
+        destroyIsland(grid, i, j + 1);
+
+    }
+}
