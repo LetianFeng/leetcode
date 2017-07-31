ff7e7ed70bd193bc9579559302e7a1b531035537
Letian Feng <letian.feng@hotmail.com>
2017-07-31 15:18:50 +0800

    Solution009 created

---
+++ src/Solution009.java
@@ -0,0 +1,20 @@
+public class Solution009 {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        if (x < 10)
+            return true;
+
+        if (x % 10 == 0)
+            return false;
+
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return (x == rev || x == rev/10);
+    }
+}
